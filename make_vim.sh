3bc4602c4bea6b56b09941bd58907638cf6d7147
Martynas Bardauskas <martynasb@wix.com>
2018-11-08 19:29:31 +0200

    steps to compile vim on mac; remove script which doesnt work

--- make_vim.sh
+++
@@ -1,7 +0,0 @@
-cd ~
-mkdir vim_source
-git clone git@github.com:vim/vim.git vim_source
-cd vim_source
-./configure --with-features=huge --enable-pythoninterp --enable-multibyte --enable-luainterp --enable-python3interp --disable-netbeans --enable-fail-if-missing
-make
-sudo make install
